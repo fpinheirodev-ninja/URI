25da1241e70c2001614ddc199ca384d268cffba5
Felipe Pinheiro <felipe.matriz@notacontrol.com.br>
2026-03-05 12:58:55 -0400

    agora vai

---
+++ iniciante/1008/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class URI
+{
+    static void Main(string[] args)
+    {
+        int employeerNumber = int.Parse(Console.ReadLine());
+        double salary =     double.Parse(Console.ReadLine());
+        double payHour =    double.Parse(Console.ReadLine());
+
+        double  result  = payHour * salary;
+
+        Console.WriteLine($"NUMBER = {employeerNumber}\nSALARY = U$ {result:f2}");
+    }
+}
