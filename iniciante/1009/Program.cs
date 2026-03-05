7b49b33bc5db572e476956cc64f24d6a6f92fc52
Felipe Pinheiro <felipe.matriz@notacontrol.com.br>
2026-03-05 12:56:24 -0400

    enviando as tarefas do uri 1007 ate 1009

---
+++ iniciante/1009/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class URI
+{
+    static void Main(string[] args)
+    {
+        string name = Console.ReadLine();
+        double salary =  double.Parse(Console.ReadLine());
+        double soldProd =  double.Parse(Console.ReadLine());
+        double tax = 0.15;
+        
+        double result = salary + (soldProd * tax);
+        Console.WriteLine($"TOTAL = R$ {result:F2}");    
+    }
+}
