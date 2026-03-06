8854ce1b7422a04e290bf7432720cfdea40c17d6
Felipe Pinheiro <felipe.matriz@notacontrol.com.br>
2026-03-06 12:59:44 -0400

    Subindo o exercicio  1010 para o git

---
+++ iniciante/1010/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class URI
+{
+    static void Main(string []args)
+    {
+       string[] produto1 = Console.ReadLine().Split(' ');
+       string[] produto2 = Console.ReadLine().Split(' ');
+
+       int codprod1 = int.Parse(produto1[0]);
+       int qtdProd1 = int.Parse(produto1[1]);
+       double priceProd1 =  double.Parse(produto1[2]);
+
+       int codprod2 = int.Parse(produto2[0]);
+       int qtdProd2 = int.Parse(produto2[1]);
+       double priceProd2 =  double.Parse(produto2[2]);
+
+
+       var result =((qtdProd1*priceProd1) + (qtdProd2*priceProd2)); 
+        Console.WriteLine($"VALOR A PAGAR: R$ {result:F2}");
+      
+
+    }
+}
